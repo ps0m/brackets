948abaa59550faaba75568a64ec9e4b46fa3ab9d
ps0m <ps0m@tut.by>
2022-02-25 00:09:26 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+ module.exports = function check(str, bracketsConfig) {
+    let openBracket = [];
+    let closeBracket = {};
+    let stack = [];
+    let currentIndexItem; 
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        openBracket.push(bracketsConfig[i][0]);
+        closeBracket[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    }
+
+    let stringArray = str.split('');
+
+    for (let i = 0; i < stringArray.length; i++) {
+
+      currentIndexItem = stringArray[i];
+
+      if (closeBracket[currentIndexItem] === currentIndexItem) {
+        if(stack.length === 0 || !(currentIndexItem === stack[stack.length-1])) {
+        stack.push(currentIndexItem);
+        } else stack.pop();
+      } else {
+        if (openBracket.includes(currentIndexItem)) {
+          stack.push(currentIndexItem);
+        } else { 
+           if(closeBracket[currentIndexItem] === (stack[stack.length-1])) {
+             stack.pop();
+           } else return false;
+        }
+      }
+      }
+
+    return (stack.length === 0)
+  }
+  
